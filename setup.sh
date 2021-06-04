99bfa2476b6e8cb9775883c1565ac25ecd8cda4a
kan-fumihito <kan_f1204@docomo.ne.jp>
2021-06-05 03:30:27 +0900

    修正

--- setup.sh
+++ setup.sh
@@ -8,4 +8,8 @@ go get -u google.golang.org/api/option
 go get -u firebase.google.com/go
 go get -u github.com/dgrijalva/jwt-go
 go get -u github.com/go-sql-driver/mysql
+
+git clone https://github.com/herumi/mcl.git
+cp mcl/ffi/go/mcl/* go/src/key_manager/pkg/mcl/ -rf
+cp mcl/ffi/go/mcl/* go/src/mcl_server/pkg/mcl/ -rf
 ls
